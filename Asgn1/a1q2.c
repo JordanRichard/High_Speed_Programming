7d5e1290cfdc44f152e906d94f4beb1c3875331a
Jordan <jordan.richard1@unb.ca>
2023-01-20 22:51:55 -0400

    Got mean calculation working with iterative approach.

--- Asgn1/a1q2.c
+++ Asgn1/a1q2.c
@@ -13,40 +13,52 @@
 
 
 /******************************************************************************
- *  Method: sumSquares: 
+ *  Method: findMean: 
  * 
  * 
- *  Input:	
- * 
- *  Output: 
+ *  Input:	FILE *input - 	File containing our data set.
+ * 			int N - 		Size of the data set to be processed.
+ *  
+ * Output:	double mk - 	Calculated mean of our data set. 
  * 
  * ****************************************************************************/
-void sumSquares()
+double mean(FILE* input,int N)
 {
-	FILE *infile = fopen("data3.txt","r");
-	int elems;
+	double mk = 0;
+	double xk = 0;
 
-	fscanf(infile,"%i", &elems);	//Finds the number of elements
-
-	int *X = (int *)malloc(elems * elems * sizeof(int));
-	
-	for(int i = 0; i < elems; i++)
+	for (int k = 1; k <= N; k++)
 	{
-		for(int j = 0; j < elems; j++)
+
+		if(k == 1)
 		{
-			int Xi;
-			fscanf(infile,"%i", &Xi);
-			*(X + i * elems + j) = Xi;
-			printf("[%i]", Xi);	
+			fscanf(input,"%lf",&mk);	//M1 
+			printf("M1: %lf\n",mk);
+		}	
+		else
+		{
+			fscanf(input,"%lf", &xk); // Reads value at posn k
+			printf("On value %lf\n",xk);
+
+			mk = mk + ((xk - mk) / k);	
+			printf("Mk: %lf\n",mk);
 		}
 	}
 
-	free(X);
-	fclose(infile);
+	return mk;
 }
 
 
 void main ()
 {
-	sumSquares();
+	FILE *infile = fopen("data3.txt","r");
+
+	int elems;
+	fscanf(infile,"%i", &elems);	//Finds the number of elements
+
+	double r = mean(infile,elems);
+
+	printf("Calculated Mean: %f\n",r);
+
+	fclose(infile);
 }
