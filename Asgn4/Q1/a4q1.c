25c28fbeafb32d385e0fc134b3d57eb4c8f8426a
user.email <jricha23@unb.ca>
2023-03-14 13:50:19 -0300

    Basically done Q1, redundant steps still there.

--- Asgn4/Q1/a4q1.c
+++ Asgn4/Q1/a4q1.c
@@ -30,39 +30,51 @@ void randomIntFrequency(int N)
     int localSum[10];
     int p,m;
 
-    //  Initialize MPI
+    // Setup MPI
     MPI_Init(NULL,NULL);
     MPI_Comm comm = MPI_COMM_WORLD;
     MPI_Comm_rank(comm, &p);
     MPI_Comm_size(comm, &m);
 
-    // Get root node to initialize input array
-    /*  -- TODO: Parallelize    */
-    if(p == ROOT)
-    {
-        srand(time(NULL));
+    int localRandoms[N/m];
 
-        //Initializes blank frequency table
+    // Get root to initialize blank frequency table
+    if(p == ROOT)      
         for(int i = 0; i < 10; i++)
             frequencyTable[i] = 0;
-
-        //Generates randomized value between 1-10 and adds to array
-        for(int i = 0; i < N; i++)
-        {
-            randomIntegers[i] = (rand() % (10-1 +1)) + 1;
-            //printf("[%d]",randomIntegers[i]);
-        }
-        //printf("\n");
+ 
+    //Seed RNG based on time and node # to ensure unique results
+    srand(time(NULL) + p);
+    
+    //printf("P%d:",p);
+    for(int i = 0; i < N/m; i++)
+    {
+        localRandoms[i] = (rand() % (10-1 +1)) + 1;
+        //printf("[%d]",localRandoms[i]);
     }
+    //printf("\n");
+    
+    // Collect generated subarrays into root array
+    MPI_Gather(localRandoms,N/m,MPI_INT,randomIntegers,N/m,MPI_INT,ROOT,comm);
+    if(p == 0)
+    {
+        printf("Gathering into root...\n");
+        //Show gathered arr at root node
+        for(int i = 0; i < N; i++)
+            printf("[%d]",randomIntegers[i]);
+        printf("\n");
+    }
+
     // Share input array with worker nodes
     MPI_Bcast(&randomIntegers,N,MPI_INT,ROOT,MPI_COMM_WORLD);
-
-	// Calculates start and endpoints of each process's subarray
+    
+    // Calculates start and endpoints of each process's subarray
 	int start = (p * (N / m));
 	int end = ((p + 1) * (N / m) -1);
+    MPI_Barrier(comm);
 
-    /*  Show subarrays  */
     /*
+    //Show subarrays
     printf("P%d: ",p);
     for(int i = start; i <= end; i++)
     {
@@ -79,19 +91,14 @@ void randomIntFrequency(int N)
 
         // Foreach value in process subarray, increment value counter if match
         for(int j = start; j <= end; j++)
-        {
-            // 
             if(randomIntegers[j] == i+1)
                 tally++;
-        }
 
         //Reduce all found value tallys to frequency table
         localSum[i] = tally;
-        MPI_Barrier(comm);
         MPI_Reduce(&localSum[i],&frequencyTable[i],1,MPI_INT,MPI_SUM,ROOT,comm);
     }
-    MPI_Barrier(comm);
-
+    
     // Gets root node to display frequency table
     if(p == ROOT)
     {
@@ -118,5 +125,5 @@ void randomIntFrequency(int N)
  * ****************************************************************************/
 int main()
 {
-    randomIntFrequency(20);
+    randomIntFrequency(8);
 }
