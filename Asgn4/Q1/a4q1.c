81479ccd034b8d822d18a8da80f1cfd45f00a1f7
jricha23 <jricha23@unb.ca>
2023-03-14 03:04:03 -0300

    (Mostly) done Q1, need to parallelize RNG and time

--- Asgn4/Q1/a4q1.c
+++ Asgn4/Q1/a4q1.c
@@ -2,12 +2,21 @@
 #include <mpi.h>
 #include <time.h>
 
-
+#define ROOT 0
+/******************************************************************************
+ * 	
+ * Driver program to create a random integer array and frequency table for 
+ * question 1.
+ *
+ *  
+ * @author	Jordan Alexander Richard
+ * @version CS 3123 - Assignment 4 Question 1
+******************************************************************************/
 
 /******************************************************************************
  *  Method: randomIntFrequency: Given a value N, generates an array of N random 
- *          integers in parallel and creates a frequency table, updated to use
- * 			OMP commands.
+ *          integers in parallel and creates a frequency table, implementation
+ *          modified to use an MPI distributed memory approach.
  * 
  *  Input:  N - The number of values to be processed.
  * 
@@ -17,60 +26,86 @@
 void randomIntFrequency(int N)
 {
     int randomIntegers[N];
-	int frequencyTable[10];
+    int p,m;
 
-	srand(time(NULL));
+    int globalSum[10];
+    int localSum[10];
 
-    int p;
-    int m;
-
-    /*  Open the parallel machine   */
+    //  Initialize MPI
     MPI_Init(NULL,NULL);
-    MPI_Comm comm = MPI_COMM_WORLD; //Shorthand
-
+    MPI_Comm comm = MPI_COMM_WORLD;
     MPI_Comm_rank(comm, &p);
     MPI_Comm_size(comm, &m);
 
-	// Calculates start and endpoints of each process
+    // Get root node to initialize input array
+    /*  -- TODO: Parallelize    */
+    if(p == ROOT)
+    {
+        srand(time(NULL));
+
+        //Initializes blank frequency table
+        for(int i = 0; i < 10; i++)
+            globalSum[i] = 0;
+
+        //Generates randomized value between 1-10 and adds to array
+        for(int i = 0; i < N; i++)
+        {
+            randomIntegers[i] = (rand() % (10-1 +1)) + 1;
+            //printf("[%d]",randomIntegers[i]);
+        }
+        //printf("\n");
+    }
+    // Share input array with worker nodes
+    MPI_Bcast(&randomIntegers,N,MPI_INT,ROOT,MPI_COMM_WORLD);
+
+
+	// Calculates start and endpoints of each process's subarray
 	int start = (p * (N / m));
 	int end = ((p + 1) * (N / m) -1);
 
-    printf("P%d S:%d E:%d\n",p,start,end);
-
-    //Generates randomized value between 1-10 and adds to array
-	for(int i = start; i <= end; i++)
-	{
-		int r = (rand() % (10-1 +1)) + 1;
-		randomIntegers[i] = r;
-        printf("Process %d making value %d and placing in randarray index %d\n",p,r,i);
-	}
-
-    // Creates frequency table of each value generated
-	for(int j = start; j <= end; j++)
-	{
-		int value = randomIntegers[j];
-		frequencyTable[value -1] = frequencyTable[value -1] + 1;
-        printf("P%d got value %d and put in box %d\n",p,value,value-1);
-	}
-
-
-    if(p == 0)
+    /*  Show subarrays  */
+    /*
+    printf("P%d: ",p);
+    for(int i = start; i <= end; i++)
     {
-        //Wipe frequency table
-        for(int i = 0; i < 10; i++)
-            frequencyTable[i] = 0;
+        printf("[%d]",randomIntegers[i]);
+    }
+    printf("\n");
+    MPI_Barrier(comm);
+    */
 
+    // Loop through each value in freq table and tally value totals by process
+    for(int i = 0; i < 10; i++)
+    {
+        int tally = 0;
+
+        // Foreach value in process subarray, increment value counter if match
+        for(int j = start; j <= end; j++)
+        {
+            // 
+            if(randomIntegers[j] == i+1)
+                tally++;
+        }
+
+        //Reduce all found value tallys to frequency table
+        localSum[i] = tally;
+        MPI_Barrier(comm);
+        MPI_Reduce(&localSum[i],&globalSum[i],1,MPI_INT,MPI_SUM,ROOT,comm);
+    }
+    MPI_Barrier(comm);
+
+    // Gets root node to display frequency table
+    if(p == ROOT)
+    {
         printf("Value:\tFrequency:\tRelative:\n");
         for(int i = 0; i < 10; i++)
         {
-            float relativeFrequency = (float)frequencyTable[i] / (float)N;
-            printf("[%i] \t%i \t\t%.4f\n", i + 1,frequencyTable[i],relativeFrequency);
+            float relativeFrequency = (float)globalSum[i] / (float)N;
+            printf("[%i] \t%i \t\t%.4f\n", i + 1,globalSum[i],relativeFrequency);
         }
     }
-
+    
     MPI_Finalize();
-    /*  Close the parallel machine  */
-
 }
 
 
