fa695ff033dd6b665a2ebdd489d17cb050334919
jricha23 <jricha23@unb.ca>
2023-03-15 14:03:10 -0300

    Progress

--- Asgn4/Q3/a4q3.c
+++ Asgn4/Q3/a4q3.c
@@ -30,11 +30,14 @@ void matrixAddition(int N)
     //double timeDifference;
 
     double arr1[N][N];
-    double arr2[N][N];
-    double arr3[N][N];
+    //double arr2[N][N];
+    //double arr3[N][N];
+    
+    double tempRow[N];
+    double otherTemp[N];
     int p,m;
 
-    double tempRow[N];
+    
 
     //  Setup MPI
     MPI_Init(NULL,NULL);
@@ -42,44 +45,76 @@ void matrixAddition(int N)
     MPI_Comm_rank(comm, &p);
     MPI_Comm_size(comm, &m);
 
+    MPI_Status status;
+
     // Calculates start and endpoints of each process
     int start = (p * (N / m));
     int end = ((p + 1) * (N / m) -1);
+    MPI_Barrier(comm);
 
+    srand(time(NULL) + p);
 
-    for(int i = start; i < end; i++)
-    {
-        printf("HI\n");
+    // for range of rows per process
+    for(int i = start; i <= end; i++)
+    {   
+        printf("P%d ",p);
+        //make input vectors  
         for(int j = 0; j < N; j++)
         {
-            //arr1[i][j] = (double)(rand() % 10);
-            //arr2[i][j] = (double)(rand() % 10);
-
-            //Generate row of data
             tempRow[j] = (double)(rand() % 10);
-            
+            printf("%.1f ",tempRow[j]);
         }
+        MPI_Barrier(comm);
+
+        
+        MPI_Gather(&tempRow,N,MPI_DOUBLE,&(arr1[i]),N,MPI_DOUBLE,ROOT,comm);
+        
+        /*
+        MPI_Send(&tempRow, N, MPI_DOUBLE, ROOT, 1, comm);
+
+        if(p == ROOT)
+        {
+            MPI_Recv(arr1, N, MPI_DOUBLE, ROOT, MPI_ANY_TAG,comm,&status);
+           
+        }
+        */
+        
+        printf("\n");
+        //printf("p%d updated row %d\n",p,i);
+        
+
         //Send the row of data we made to the root matrix somehow
-        MPI_Gather(&tempRow,N,MPI_DOUBLE,&arr1[i],N,MPI_DOUBLE,ROOT,comm);
-        printf("[%f]",tempRow[i]);
+        
+        //printf("p%d iter%d sending to row %d\n",p,i);
+        MPI_Barrier(comm);
+        
+        
     }
     printf("\n");
+    MPI_Barrier(comm);
 
+    
     // Get root to print result matrix
    if(p == ROOT)
    {
+        //for(int x=0;x<N;x++)
+        //{
+        //    arr1[7][x] = 999;
+        //}
+        printf("Back on 0, printing arr...\n");
         for(int i = 0; i < N; i++)
         {
 
-            printf("P%d:",p);
+            printf("Row%d:",i);
             for(int j = 0; j < N; j++)
             {
-                printf("[%f]",arr1[i][j]);
+                printf("[%.0f]",arr1[i][j]);
             }
             printf("\n");  
         }
 
    }
+   
 
 }
 
@@ -94,5 +129,5 @@ void matrixAddition(int N)
  * ****************************************************************************/
 int main()
 {
-    matrixAddition(4);
+    matrixAddition(8);
 }
